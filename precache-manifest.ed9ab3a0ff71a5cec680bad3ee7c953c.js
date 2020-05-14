4a73ee7c59478248b2e6bd66be6454408f15fa59
Aravinth-thunder <varavinth321@gmail.com>
2020-05-14 21:35:53 +0530

    Updates

---
+++ precache-manifest.ed9ab3a0ff71a5cec680bad3ee7c953c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb5beb8acdad1ad11e88cbbbee70c48",
+    "url": "/todo_with_login/index.html"
+  },
+  {
+    "revision": "99b7d1547249563cd032",
+    "url": "/todo_with_login/static/css/main.368772d2.chunk.css"
+  },
+  {
+    "revision": "27f76acba3b79ea2537b",
+    "url": "/todo_with_login/static/js/2.23b727a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_with_login/static/js/2.23b727a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b7d1547249563cd032",
+    "url": "/todo_with_login/static/js/main.302fa1a8.chunk.js"
+  },
+  {
+    "revision": "66de78fe149878ab8562",
+    "url": "/todo_with_login/static/js/runtime-main.619112c8.js"
+  }
+]);
